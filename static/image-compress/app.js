e121891a08ecd35cc8d97a0b1fe6e9b16a1b62ae
Fadhil Yori <me@fadhilyori.my.id>
2023-08-08 21:17:47 +0700

    fix dark mode

--- static/image-compress/app.js
+++ static/image-compress/app.js
@@ -1,3 +1,13 @@
+const htmlElement = document.querySelector("html")
+if (htmlElement.getAttribute("data-bs-theme") === 'auto') {
+    function updateTheme() {
+        document.querySelector("html").setAttribute("data-bs-theme",
+            window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
+    }
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme)
+    updateTheme()
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const fileInput = document.getElementById('fileInput');
     const qualityValue = document.getElementById('qualityValue');
