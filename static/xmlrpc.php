3af1ff76cb1375cd2b9d6df815dccc179b414083
Fadhil Yori <me@fadhilyori.my.id>
2023-08-30 12:18:46 +0700

    add xmlrpc.php

---
+++ static/xmlrpc.php
@@ -0,0 +1,1 @@
+<html><head><meta name="color-scheme" content="light dark"></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">XML-RPC server accepts POST requests only.</pre></body></html>
